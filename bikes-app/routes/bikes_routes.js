79af4ae5ccaf578018b1c08baa2de7a3cbd1f92a
nbondarenko <nataliya.bondarenko17@gmail.com>
2018-05-10 00:01:15 +0300

    last changes

--- bikes-app/routes/bikes_routes.js
+++ bikes-app/routes/bikes_routes.js
@@ -1,5 +1,6 @@
 var ObjectID = require('mongodb').ObjectID;
 const moment = require('moment');
+var request = require('request');
 const DB_COLLECTION = 'bikes';
 
 module.exports = function (app, db) {
@@ -15,7 +16,7 @@ module.exports = function (app, db) {
 
   app.post('/bikes', (request, response) => {
     const bike = {
-      color: request.body.name,
+      color: request.body.color,
       type: request.body.type,
       available_from: moment(request.body.available_from).format('YYYY-MM-DD'),
     };
@@ -56,7 +57,7 @@ module.exports = function (app, db) {
     const id = request.params.id;
     const details = { '_id': new ObjectID(id) };
     const bike = {
-      color: request.body.name,
+      color: request.body.color,
       type: request.body.type,
       available_from: moment(request.body.available_from).format('YYYY-MM-DD'),
     };
@@ -65,6 +66,39 @@ module.exports = function (app, db) {
         response.send({ 'error': 'An error has occurred' });
       } else {
         response.send(bike);
+      }
+    });
+  });
+
+  app.post('/bikes/:id/book', (myRequest, response) => {
+    const id = myRequest.params.id;
+    const availableFrom = moment(myRequest.body.to_time).format('YYYY-MM-DD');
+    const amount = myRequest.body.amount;
+    const details = { '_id': new ObjectID(id) };
+    const action = {
+      $set: { available_from: availableFrom }
+    }
+    const options = { 'new': true }
+    db.collection(DB_COLLECTION).findAndModify(details, [['_id', 1]], action, options, (err, item) => {
+      if (err) {
+        response.send({ 'error': 'An error has occurred' });
+      } else {
+        var options = {
+          uri: `http://bookingapp:5001/bookings`,
+          method: 'POST',
+          json: {
+            'bike_id': id,
+            'amount': amount
+          }
+        };
+
+        request(options, function (error, r, body) {
+          if (!error && r.statusCode == 200) {
+            response.send(item.value);
+          } else {
+            response.send({ 'error': 'An error has occurred' });
+          }
+        });
       }
     });
   });
--- bookings-app/config.ru
+++ bookings-app/config.ru
@@ -2,6 +2,7 @@ $LOAD_PATH << File.join(File.dirname(__FILE__))
 
 require 'rubygems'
 require 'bundler'
+require 'net/http'
 Bundler.require(:default)
 require './app'
 
